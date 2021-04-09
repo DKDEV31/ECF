cac91bfb3c9557d2a02952c00052d0a94ecbf3b7
Benjamin Faucheux <benjaminfaucheux40@gmail.com>
2021-04-09 20:53:26 +0200

    Animation dropdown

--- assets/app.js
+++ assets/app.js
@@ -8,4 +8,21 @@
 // any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
 import './styles/Home/home-content.scss';
+import {gsap} from 'gsap'
 
+const navMenuToggle = document.querySelector('.dropdown-menu')
+const navMenuBlock = document.querySelector('.navToggleMenu')
+
+navMenuToggle.addEventListener('click', ()=>{
+    if(navMenuBlock.classList.contains('active')) {
+        navMenuToggle.classList.remove('active')
+        navMenuToggle.classList.add('desactive')
+        navMenuBlock.classList.remove('active')
+        navMenuBlock.classList.add('desactive')
+    } else {
+        navMenuToggle.classList.add('active')
+        navMenuBlock.classList.add('active')
+        // navMenuBlock.classList.remove('desactive')
+        // navMenuToggle.classList.remove('desactive')
+    }
+})
