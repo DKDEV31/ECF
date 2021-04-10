faa31d48d6ae1a11c669279ebe6b0fe47411df7e
Benjamin Faucheux <benjaminfaucheux40@gmail.com>
2021-04-10 14:16:04 +0200

    Création du controller et du formulaire pour la page register

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use App\Security\MainAuthenticator;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, MainAuthenticator $authenticator): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //format the adress field
+            $adress = sprintf('%s-%s-%s',
+                $form->get('adress')->getData(),
+                $form->get('zipcode')->getData(),
+                $form->get('city')->getData()
+            );
+            $user->setAdress($adress);
+            // encode the plain password
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            return $guardHandler->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $authenticator,
+                'main' // firewall name in security.yaml
+            );
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname', TextType::class,[
+              'label' => 'Prénom',
+                'row_attr' =>['class' => 'form-row'],
+                'constraints' => [
+
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'row_attr' => ['class' => 'form-row'],
+                'constraints' => [
+
+                ]
+            ])
+            ->add('email', EmailType::class,[
+                'label' => 'Email',
+                'row_attr' => ['class' => 'form-email'],
+                'constraints' => [
+
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'mapped' => false,
+                'type' => PasswordType::class,
+                'first_name' => 'password',
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'row_attr' => ['class' => 'form-password']
+                ],
+                'second_name' => 'confirmPassword',
+                'second_options' => [
+                    'label' => 'Confirmation du mot de passe',
+                    'row_attr' => ['class' => 'form-password']
+                ],
+
+                'constraints' => [
+
+                ]
+            ])
+            ->add('adress', TextType::class,[
+                'label' => 'Adresse',
+                'mapped' => false,
+                'row_attr' => ['class' => 'form-adress'],
+                'constraints' => [
+
+                ]
+            ])
+            ->add('zipcode', TextType::class, [
+                'label' => 'Code Postal',
+                'row_attr' => ['class' => 'form-zipcode'],
+                'mapped' => false,
+                'constraints' => [
+
+                ]
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville',
+                'row_attr' => ['class' => 'form-city'],
+                'mapped' => false,
+                'constraints' => [
+
+                ]
+            ])
+            ->add('phone', TelType::class, [
+                'label' => 'Telephone',
+                'row_attr' => ['class' => 'form-phone'],
+                'constraints' => [
+
+                ]
+            ])
+            ->add('birthDate', BirthdayType::class, [
+                'label' => 'Date de naissance',
+                'view_timezone' => 'Europe/Paris',
+                'widget' => 'single_text',
+                'row_attr' => ['class' => 'form-birthdate'],
+                'constraints' => [
+
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'S\'inscrire',
+                'attr' => ['class' => 'btn'],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
