2dceff44460ba31d657865ea0e8c041df5ff0c22
Benjamin Faucheux <benjaminfaucheux40@gmail.com>
2021-04-14 20:09:19 +0200

    Suppression des commentaires

--- src/Repository/BenefitRepository.php
+++ src/Repository/BenefitRepository.php
@@ -19,32 +19,5 @@ class BenefitRepository extends ServiceEntityRepository
         parent::__construct($registry, Benefit::class);
     }
 
-    // /**
-    //  * @return Benefit[] Returns an array of Benefit objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('b')
-            ->andWhere('b.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('b.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
 
-    /*
-    public function findOneBySomeField($value): ?Benefit
-    {
-        return $this->createQueryBuilder('b')
-            ->andWhere('b.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
